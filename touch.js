1fbc6a8ecec631c10e3b4b0d8150031d7813bc89
Noortje Hopman <noor.hop@gmail.com>
2020-07-08 19:51:17 +0100

    prevent attitude

--- touch.js
+++ touch.js
@@ -10,8 +10,20 @@ let touching = {
     is: false
 }
 
+let timeout
+
 button.on('alert', (level) => {
-    touching.is = !!level
+
+	if(!level){ //not touching
+		timeout = setTimeout(() => {
+			touching.is = false
+		}, 2000)
+	}
+	else { //touching
+		clearTimeout(timeout)
+		touching.is = true
+	}
+    //touching.is = !!level
 });
 
 const isTouching = () => touching.is
